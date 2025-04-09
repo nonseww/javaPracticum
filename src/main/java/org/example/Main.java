0676cf6448941bf34b102b1e51d9ec1984a32f48
nonseww <innapolituchaya@gmail.com>
2025-04-09 15:32:31 +0400

    Added find function, formatted print info in Cities class
    ADDED: find function that returns City object with a desired name,
    formatCityInfo func for formatting console.out of all the cities

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,18 @@
 package org.example;
 
+import org.example.model.Cities.Cities;
+import org.example.model.City.City;
+
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
         try {
+            City city1 = new City("Саратов");
+            City city2 = new City("Москва");
+            Cities cities = new Cities(city1, city2);
+            System.out.println(cities);
         }
         catch (Exception e) {
             System.out.println(e.getMessage());
--- src/main/java/org/example/model/Cities/Cities.java
+++ src/main/java/org/example/model/Cities/Cities.java
@@ -3,6 +3,9 @@ package org.example.model.Cities;
 import org.example.model.City.City;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * Класс представляет собой список объектов класса City.
@@ -13,16 +16,47 @@ import java.util.ArrayList;
 public class Cities {
     private ArrayList<City> cityList = new ArrayList<>();
     /**
-     * При создании объявляет пустой ArrayList
+     * При создании объявляет пустой ArrayList.
+     * Инициализацию можно провести без параметров, с одним City или с ArrayList<City>.
      */
+
+
+    private ArrayList<City> getAllCities() {
+        return cityList;
+    }
 
     public Cities() {}
     public Cities(City city) {
+        this();
         this.cityList.add(city);
     }
-    public Cities(ArrayList<City> cities) {
+    public Cities(City ...cities) {
+        this();
         if (cities != null) {
-            this.cityList.addAll(cities);
+            this.cityList.addAll(List.of(cities));
         }
+    }
+
+    public City find(String name) {
+        return cityList.stream()
+                .filter(city -> Objects.equals(city.getName(), name))
+                .findFirst()
+                .orElse(null);
+    }
+
+//    public ArrayList<City> find(int temperature) {
+//        return cityList.stream()
+//                .filter(city -> Objects.equals(city.getTemperature(), temperature));
+//    }
+
+    private String formatCityInfo(City city) {
+        return String.format("Город %s, температура сейчас %d°C", city.getName(), city.getTemperature());
+    }
+
+    @Override
+    public String toString() {
+        return getAllCities().stream()
+                .map(this::formatCityInfo)
+                .collect(Collectors.joining("\n"));
     }
 }
