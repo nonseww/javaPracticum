71893358d40712d13d231cd82023d023b4be19db
nonseww <innapolituchaya@gmail.com>
2025-04-10 18:59:58 +0400

    Created the text for user
    ADDED: data.txt with text for user is in resources now. Writes by
    InputStream and Scanner

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,28 +2,21 @@ package org.example;
 
 import org.example.model.Cities.Cities;
 import org.example.model.City.City;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
-import java.util.ArrayList;
 
 public class Main {
-
     public static void main(String[] args) {
-        try {
-            City city1 = new City("Саратов");
-            City city2 = new City("Москва");
-            City city3 = new City("Питер");
-            Cities cities = new Cities(city1);
-            cities.add(city2, city3);
-            City city4 = new City("Астрахань");
-            cities.add(city4);
-            System.out.println(cities);
-        }
-        catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    public static void addCity(String name) {
-
+            InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("data/data.txt");
+            if (inputStream != null) {
+                try (Scanner scanner = new Scanner(inputStream, StandardCharsets.UTF_8)) {
+                    while(scanner.hasNextLine()) {
+                        String line = scanner.nextLine();
+                        System.out.println(line);
+                    }
+                }
+            }
     }
 }
